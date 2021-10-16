8ec69e12ece0054ac3e74ce651c9f89c4db309d7
Dragon77mathbye <apgillen2016@gmail.com>
2021-10-16 15:47:41 -0400

    asf

--- toNumberName.js
+++ toNumberName.js
@@ -1,9 +1,10 @@
+let spreadElements = ["deci", "viginti", "triginta", "quadraginta", "quinquaginta", "sexaginta", "septuaginta", "octoginta", "nonaginta"];
 var numberNames = [
     ["","milli","billi","trilli","quadrilli","quintilli","sextilli","septilli","octilli","nonilli","decilli"],
     ["novem","","un","duo","tre","quattuor","quin","sex","septen","octo"],
     ["","decillion","vigintillion","trigintillion","quadragintillion","quinquagintillion","sexagintillion","septuagintillion","octogintillion","nonagintillion"],
     ["","centillion","ducentillion","trucentillion","quadringentillion","quingentillion","sescentillion","septingentillion","octingentillion","nongentillion"],
-    ["","deci","viginti","triginta","quadraginta","quinquaginta","sexaginta","septuaginta","octoginta","nonaginta"],
+    ["", ...spreadElements],
     ["K","million","billion","trillion","quadrillion","quintillion","sextillion","septillion","octillion","nonillion"]
 ];
 function toNumberName(number, type) {
@@ -15,10 +16,28 @@ function toNumberName(number, type) {
 	} else if (type === undefined) {
 		type = "default";
 	} else {
-		return 'Type can only be "expantanum", "decimal", or "default" (with quotes)';
+		console.error('Type can only be "expantanum", "decimal", or "default" (with quotes)');
 	}
 	if (type === "decimal" || type === "expantanum") {
-		numberName[0] = numberNames[0][((number.abs().log10().sub(3)).div(3000)).floor().toNumber()];
+		let illion = number.abs().log10().sub(3);
+		numberName[0] = numberNames[0][(illion.div(3000)).floor().toNumber()];
+		if (number.abs().log10().toNumber()%3000 >= 33) {
+			numberName[1] = numberNames[1][((number.abs().log10().div(3)).floor()).toNumber()%10];
+		} else {
+			if (number.abs().log10().greaterThanOrEqualTo(3003)) {
+				numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor()).toNumber()%10];
+			} else {
+				numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor().sub(1)).toNumber()%10];
+			}
+		}
+		let illionSegment = ((illion.div(30)).floor()).toNumber()%10;
+		if (number.abs().log10().toNumber()%3000 >= 303) {
+			numberName[2] = numberNames[4][illionSegment];
+		} else {
+			numberName[2] = numberNames[2][illionSegment];
+		}
+		numberName[3] = numberNames[3][((illion.div(300)).floor()).toNumber()%10];
+		numberName[0] = numberNames[0][(illion.div(3000)).floor().toNumber()];
 		if (number.abs().log10().toNumber()%3000 >= 33) {
 			numberName[1] = numberNames[1][((number.abs().log10().div(3)).floor()).toNumber()%10];
 		} else {
@@ -29,35 +48,19 @@ function toNumberName(number, type) {
 			}
 		}
 		if (number.abs().log10().toNumber()%3000 >= 303) {
-			numberName[2] = numberNames[4][(((number.abs().log10().sub(3)).div(30)).floor()).toNumber()%10];
+			numberName[2] = numberNames[4][illionSegment];
 		} else {
-			numberName[2] = numberNames[2][(((number.abs().log10().sub(3)).div(30)).floor()).toNumber()%10];
+			numberName[2] = numberNames[2][illionSegment];
 		}
-		numberName[3] = numberNames[3][(((number.abs().log10().sub(3)).div(300)).floor()).toNumber()%10];
-		numberName[0] = numberNames[0][((number.abs().log10().sub(3)).div(3000)).floor().toNumber()];
-		if (number.abs().log10().toNumber()%3000 >= 33) {
-			numberName[1] = numberNames[1][((number.abs().log10().div(3)).floor()).toNumber()%10];
-		} else {
-			if (number.abs().log10().greaterThanOrEqualTo(3003)) {
-				numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor()).toNumber()%10];
-			} else {
-				numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor().sub(1)).toNumber()%10];
-			}
-		}
-		if (number.abs().log10().toNumber()%3000 >= 303) {
-			numberName[2] = numberNames[4][(((number.abs().log10().sub(3)).div(30)).floor()).toNumber()%10];
-		} else {
-			numberName[2] = numberNames[2][(((number.abs().log10().sub(3)).div(30)).floor()).toNumber()%10];
-		}
-		numberName[3] = numberNames[3][(((number.abs().log10().sub(3)).div(300)).floor()).toNumber()%10];
+		numberName[3] = numberNames[3][((illion.div(300)).floor()).toNumber()%10];
 		if (number.log10().toNumber() >= 33000) {
 			return number.toExponential(3);
 		} else if (number.abs().log10().floor().toNumber() >= 3003 && (number.abs().log10().floor().sub(3)).toNumber()%3000 >= 0 && (number.abs().log10().floor().sub(3)).toNumber()%3000 <= 2) {
-			return num.pow((number.abs().log10()).sub(number.abs().log10().floor())).mul(num.pow(number.abs().log10().floor().toNumber()%3)).mul(number.div(number.abs())).toFixed(3) + " " + numberName[0] + "llion";
+			return first() + " " + numberName[0] + "llion";
 		} else if (number.abs().log10().floor().greaterThanOrEqualTo(3)) {
-			return num.pow((number.abs().log10()).sub(number.abs().log10().floor())).mul(num.pow(number.abs().log10().floor().toNumber()%3)).mul(number.div(number.abs())).toFixed(3) + " " + numberName[0] + numberName[1] + numberName[2] + numberName[3];
+			return first() + " " + numberName[0] + numberName[1] + numberName[2] + numberName[3];
 		} else if (number.greaterThanOrEqualTo(1)) {
-			return num.pow((number.abs().log10()).sub(number.abs().log10().floor())).mul(num.pow(number.abs().log10().floor().toNumber()%3)).mul(number.div(number.abs())).toFixed(3);
+			return first();
 		} else {
 			return number.toString();
 		}
@@ -105,5 +108,9 @@ function toNumberName(number, type) {
 		} else {
 			return number;
 		}
+	}
+
+	function first() {
+		return num.pow((number.abs().log10()).sub(number.abs().log10().floor())).mul(num.pow(number.abs().log10().floor().toNumber() % 3)).mul(number.div(number.abs())).toFixed(3);
 	}
 }
