fb6c828eff02692a96d567399dec09b475df17ed
Dragon77mathbye <apgillen2016@gmail.com>
2021-12-17 16:15:26 -0500

    completely rewritten code

--- README.md
+++ README.md
@@ -3,32 +3,38 @@ Converts a number to the "illions" name.
 
 Use function toNumberName(number)
 
-Replace "number" with an ExpantaNum number, break infinity/eternity/decimal number, or basic javascript number
+Replace "number" with an ExpantaNum, OmegaNum, break infinity/eternity/Decimal, or basic javascript number or string
 
-Goes up to 10^33000
+Goes up to 10^3000003 (Micrillion)
 
 # How to use
 
 There is a function called toNumberName() that is used to convert the number.
 
-It has four parameters: `toNumberName(number, type, abbreviate, decimalPlaces)`
+It has three parameters: `toNumberName(number, abbreviate, decimalPlaces)`
 
 `number` is the number you want to convert
 
-`type` is the type of number (used for compatibility with other libraries such as break_infinity.js
-
 `abbreviate` is a simple true/false parameter. It controls whether you want the numbers to be abbreviated, like 2 billion turns to 2 B.
 
-`decimalPlaces` is a parameter to print how many digits are after the decimal point, for example it would write it as 2.38 (2) or 2.3815 (4)
+`decimalPlaces` is a parameter to print how many digits are after the decimal point, for example it would write it as 2.38 (2) or 2.3815 (4). You can set this to -1 to not have a fixed decimal count.
 
 Example for how to use:
 
 ```js
-
 let number = 2.8e108;
-> toNumberName(number);
-< "2.800 quintrigintillion"
 
-> toNumberName(number, "default", true, 1);
-< "2.8 QiTg"
+toNumberName(number);
+"2.800 quintrigintillion"
+
+toNumberName(number, true, 1);
+"2.8 QiTg"
+
+number = "1.63e54321";
+toNumberName(number, false, -1);
+"1.63 septendecimilli-sexcentillion"
+
+number = "768328947378913428973142678342169734196743916743671483291673428"
+toNumberName(number, true, -1);
+"768.3289473789134 NoDc"
 ```
--- toNumberName.js
+++ toNumberName.js
@@ -1,141 +1,67 @@
-function toNumberName(number, type, abbreviate, decimalPlaces) {
-    let numberNames;
+function toNumberName(number, abbreviate, decimalPlaces, dontChange, dontChange1) {
+    const arrays = [
+        ["", "un", "duo", "tre", "quattuor", "quin", "sex", "septen", "octo", "novem"],
+        ["", "mi", "bi", "tri", "quadri", "quinti", "sexti", "septi", "octi", "noni"],
+        ["", "deci", "viginti", "triginti", "quadraginti", "quinquaginti", "sexaginti", "septuaginti", "octoginti", "nonaginti"],
+        ["", "centi", "ducenti", "trucenti", "quadringenti", "quingenti", "sescenti", "septingenti", "octingenti", "nongenti"],
+        ["", "milli-", "micro-", "nano-", "pico-", "femto-", "atto-", "zepto-", "yocto-", "xenno-", "weko-", "vendeko-"],
+        ["", "milli", "micri", "nani", "pici", "femti", "atti", "zepti", "yocti", "xenni", "weki", "vendeki"],
+        ["", "U", "D", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No"],
+        ["", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No"],
+        ["", "Dc", "Vg", "Tg", "Qag", "Qig", "Sxg", "Spg", "Ocg", "Nog"],
+        ["", "Ct", "Dct", "Tct", "Qagt", "Qigt", "Ssct", "Spgt", "Ocgt", "Nogt"],
+        ["", "MI-", "μ-", "N-", "P-", "F-", "A-", "Z-", "Y-", "X-", "W-", "V-"],
+        ["", "MI", "μ", "N", "P", "F", "A", "Z", "Y", "X", "W", "V"]
+    ];
+    number = number.toString();
     if (decimalPlaces === undefined) {
         decimalPlaces = 3;
-    } else if (decimalPlaces > 14) {
-        console.warn("decimalPlaces has a max value of 14");
     }
     if (abbreviate) {
-        numberNames = [
-            ["","M","B","T","Qa","Qi","Sx","Sp","Oc","No","Dc"],
-            ["N","","U","D","T","Qa","Qi","Sx","Sp","Oc"],
-            ["","Dc","Vg","Tg","Qag","Qig","Sxg","Spg","Ocg","Nog"],
-            ["","Ct","Dct","Tct","Qagt","Qigt","Sxct","Spgt","Ocgt","Nogt"],
-            ["", "Dc", "Vg", "Tg", "Qag", "Qig", "Sxg", "Spg", "Ocg", "Nog"],
-            ["K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No"]
-        ];
+        abbreviate = 6;
     } else {
-        numberNames = [
-            ["","milli","billi","trilli","quadrilli","quintilli","sextilli","septilli","octilli","nonilli","decilli"],
-            ["novem","","un","duo","tre","quattuor","quin","sex","septen","octo"],
-            ["","decillion","vigintillion","trigintillion","quadragintillion","quinquagintillion","sexagintillion","septuagintillion","octogintillion","nonagintillion"],
-            ["","centillion","ducentillion","trucentillion","quadringentillion","quingentillion","sescentillion","septingentillion","octingentillion","nongentillion"],
-            ["", "deci", "viginti", "triginta", "quadraginta", "quinquaginta", "sexaginta", "septuaginta", "octoginta", "nonaginta"],
-            ["K","million","billion","trillion","quadrillion","quintillion","sextillion","septillion","octillion","nonillion"]
-        ];
+        abbreviate = 0;
     }
-    let numberName = ["","","",""];
-    let num;
-    if (type === "expantanum") {
-        num = ExpantaNum(10);
-    } else if (type === "decimal") {
-        num = new Decimal(10);
-    } else if (type === undefined || type === "default") {
-        type = "default";
+    let output = ["", "", "", ""];
+    let exponent;
+    let mantissa;
+    if (number.match("e") !== null) {
+        exponent = Number(number.split("e")[1]);
+        if (decimalPlaces !== -1) {
+            mantissa = (Number(number.split("e")[0]) * 10 ** (exponent % 3)).toFixed(decimalPlaces);
+        } else {
+            mantissa = (Number(number.split("e")[0]) * 10 ** (exponent % 3))
+        }
     } else {
-        console.error('Type can only be "expantanum", "decimal", or "default" (with quotes, leave blank for default)');
-    }
-    let exponent = Math.floor(Math.log10(Math.abs(number)) / 3);
-    if (type === "decimal" || type === "expantanum") {
-        let illion = number.abs().log10().sub(3);
-        numberName[0] = numberNames[0][(illion.div(3000)).floor().toNumber()];
-        if (number.abs().log10().toNumber()%3000 >= 33) {
-            numberName[1] = numberNames[1][((number.abs().log10().div(3)).floor()).toNumber()%10];
+        exponent = number.split(".")[0].length - 1;
+        if (decimalPlaces !== -1) {
+            mantissa = (Number(number.slice(0, 21)) / 10 ** (number.split(".")[0].slice(0, 21).length - exponent % 3 - 1)).toFixed(decimalPlaces);
         } else {
-            if (number.abs().log10().greaterThanOrEqualTo(3003)) {
-                numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor()).toNumber()%10];
-            } else {
-                numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor().sub(1)).toNumber()%10];
-            }
-        }
-        let illionSegment = ((illion.div(30)).floor()).toNumber()%10;
-        if (number.abs().log10().toNumber()%3000 >= 303) {
-            numberName[2] = numberNames[4][illionSegment];
-        } else {
-            numberName[2] = numberNames[2][illionSegment];
-        }
-        numberName[3] = numberNames[3][((illion.div(300)).floor()).toNumber()%10];
-        numberName[0] = numberNames[0][(illion.div(3000)).floor().toNumber()];
-        if (number.abs().log10().toNumber()%3000 >= 33) {
-            numberName[1] = numberNames[1][((number.abs().log10().div(3)).floor()).toNumber()%10];
-        } else {
-            if (number.abs().log10().greaterThanOrEqualTo(3003)) {
-                numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor()).toNumber()%10];
-            } else {
-                numberName[1] = numberNames[5][((number.abs().log10().div(3)).floor().sub(1)).toNumber()%10];
-            }
-        }
-        if (number.abs().log10().toNumber()%3000 >= 303) {
-            numberName[2] = numberNames[4][illionSegment];
-        } else {
-            numberName[2] = numberNames[2][illionSegment];
-        }
-        numberName[3] = numberNames[3][((illion.div(300)).floor()).toNumber()%10];
-        if (number.log10().toNumber() >= 33000) {
-            return number.toExponential(3);
-        } else if (number.abs().log10().floor().toNumber() >= 3003 && (number.abs().log10().floor().sub(3)).toNumber()%3000 >= 0 && (number.abs().log10().floor().sub(3)).toNumber()%3000 <= 2) {
-            return first() + " " + numberName[0] + "llion";
-        } else if (number.abs().log10().floor().greaterThanOrEqualTo(3)) {
-            return first() + " " + numberName[0] + numberName[1] + numberName[2] + numberName[3];
-        } else if (number.greaterThanOrEqualTo(1)) {
-            return first();
-        } else {
-            return number.toString();
-        }
-    } else if (type === "default") { // Plain Javascript
-        numberName[0] = numberNames[0][Math.floor((Math.log10(Math.abs(number)) - 3) / 3000)];
-        if (Math.log10(Math.abs(number))%3000 >= 33) {
-            numberName[1] = numberNames[1][exponent%10];
-        } else {
-            if (Math.log10(Math.abs(number)) >= 3003) {
-                numberName[1] = numberNames[5][exponent%10];
-            } else {
-                numberName[1] = numberNames[5][(exponent - 1)%10];
-            }
-        }
-        if (Math.log10(Math.abs(number))%3000 >= 303) {
-            numberName[2] = numberNames[4][(Math.floor((Math.log10(Math.abs(number)) - 3) / 30))%10];
-        } else {
-            numberName[2] = numberNames[2][(Math.floor((Math.log10(Math.abs(number)) - 3) / 30))%10];
-        }
-        numberName[3] = numberNames[3][(Math.floor((Math.log10(Math.abs(number)) - 3) / 300))%10];
-        numberName[0] = numberNames[0][Math.floor((Math.log10(Math.abs(number)) - 3) / 3000)];
-        if (Math.log10(Math.abs(number))%3000 >= 33) {
-            numberName[1] = numberNames[1][exponent%10];
-        } else {
-            if (Math.log10(Math.abs(number)) >= 3003) {
-                numberName[1] = numberNames[5][exponent%10];
-            } else {
-                numberName[1] = numberNames[5][(exponent - 1)%10];
-            }
-        }
-        if (Math.log10(Math.abs(number))%3000 >= 303) {
-            numberName[2] = numberNames[4][(Math.floor((Math.log10(Math.abs(number)) - 3) / 30))%10];
-        } else {
-            numberName[2] = numberNames[2][(Math.floor((Math.log10(Math.abs(number)) - 3) / 30))%10];
-        }
-        numberName[3] = numberNames[3][(Math.floor((Math.log10(Math.abs(number)) - 3) / 300))%10];
-
-        let mainOutput = (Math.round((10**((Math.log10(Math.abs(number))) - (Math.floor(Math.log10(Math.abs(number))))) * (10**(Math.floor(Math.log10(Math.abs(number)))%3)) * (number / (Math.abs(number)))) * (10**decimalPlaces)) / (10**decimalPlaces)).toFixed(decimalPlaces);
-
-        if (Math.log10(number) >= 33000) {
-            return "Infinity";
-        } else if (Math.floor(Math.log10(Math.abs(number))) >= 3003 && (Math.floor(Math.log10(Math.abs(number))) - 3)%3000 >= 0 && (Math.floor(Math.log10(Math.abs(number))) - 3)%3000 <= 2) {
-            if (abbreviate) {
-                return mainOutput + " " + numberName[0] + "L";
-            } else {
-                return mainOutput + " " + numberName[0] + "llion";
-            }
-        } else if (Math.floor(Math.log10(Math.abs(number))) >= 3) {
-            return mainOutput + " " + numberName[0] + numberName[1] + numberName[2] + numberName[3];
-        } else if (number >= 1) {
-            return mainOutput;
-        } else {
-            return number;
+            mantissa = (Number(number.slice(0, 21)) / 10 ** (number.split(".")[0].slice(0, 21).length - exponent % 3 - 1));
         }
     }
-
-    function first() {
-        return num.pow((number.abs().log10()).sub(number.abs().log10().floor())).mul(num.pow(number.abs().log10().floor().toNumber() % 3)).mul(number.div(number.abs())).mul(10**decimalPlaces).floor().div(10**decimalPlaces).toFixed(decimalPlaces);
+    output[0] = ((exponent >= 3003) ? toNumberName(`1e${exponent / 10 ** ~~((~~(Math.log10(exponent / 3 - 1) / 3)) * 3)}`, abbreviate, decimalPlaces, exponent >= 3003, true) : "") + ((exponent % 3000 >= 3 && exponent % 3000 < 6) ? "" : arrays[4 + abbreviate][~~(Math.log10(exponent / 3 - 1) / 3)]);
+    if (exponent % 3000 >= 3 && exponent % 3000 < 6) {
+        output[1] = arrays[5 + abbreviate][~~(Math.log10(exponent / 3 - 1) / 3)];
+    }
+    if ((exponent - 3) % 3000 < 33 && !dontChange1) {
+        output[2] = arrays[1 + abbreviate][~~((exponent - 3) % 30 / 3)];
+        if (exponent >= 3 && exponent < 6) {
+            output[2] = abbreviate ? "K" : "thousand";
+        }
+    } else {
+        output[2] = arrays[abbreviate][~~((exponent - 3) % 30 / 3)];
+    }
+    output[3] = arrays[2 + abbreviate][~~((exponent - 3) % 300 / 30)];
+    output[4] = arrays[3 + abbreviate][~~((exponent - 3) % 3000 / 300)];
+    if (dontChange) {
+        mantissa = "";
+    } else {
+        mantissa += " ";
+    }
+    if (exponent > 3000003) {
+        return number;
+    } else {
+        return `${mantissa}${output.join("")}${(abbreviate || exponent < 6 || dontChange) ? "" : "llion"}`.replaceAll("undefined", "");
     }
 }
